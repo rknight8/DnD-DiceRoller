37b5152a155555a6ecd4d52239a8294ddb7828f9
Mike Hart <michael-hart@live.com>
2019-06-18 10:47:50 +0100

    Get functions Button hooked with functions

--- roller.js
+++ roller.js
@@ -1,6 +1,7 @@
 "use strict";
 
-const textInput = document.getElementById("text-input");
+const textInput = document.getElementById("user-input");
+const outputBox = document.getElementById("output-box");
 
 // Rolling function
 function rollDice(count, max) {
@@ -17,20 +18,28 @@ function rollDice(count, max) {
             console.log("Rolled " + currentRoll);
         }
         console.log("Total: " + totalRoll);
+        outputBox.innerHTML = "\nTotal: " + totalRoll + outputBox.innerHTML;
     }
   }
+
+// Get data and pass to roll function
+const getRoll = () => {
+    const rollData = textInput.value.split("d");
+    if (rollData.length === 2 ) {
+        rollDice(rollData[0], rollData[1]);
+    }
+    else {
+        console.log("Please specify only one dice. Such as 2d6, or 1d20");
+    }
+}
+
 
 // Keyboard event
 const enterEvent = (e) => {
     if (e.keyCode === 13) {
-        const rollData = textInput.value.split("d");
-        if (rollData.length === 2 ) {
-            rollDice(rollData[0], rollData[1]);
-        }
-        else {
-            console.log("Please specify only one dice. Such as 2d6, or 1d20");
-        }
+        getRoll();
     }
 }
+// Listen for the enter key on text input
 textInput.addEventListener("keypress", enterEvent);
 
